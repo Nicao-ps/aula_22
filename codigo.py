c1a50841dbf5715d5d22c4fbe54364b3ffa57546
Nicao-ps <nps.guimaraes@gmail.com>
2025-07-01 19:09:57 -0300

    introduz codigo

---
+++ codigo.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+print('Coletando dados para análise...')
+
+CAMINHO_ARQUIVO_1 = 
+df_serie_1 = pd.read_csv(CAMINHO_ARQUIVO_1 , sep=';', encoding='iso-8859-1')
+
+CAMINHO_ARQUIVO_2 = 
+df_serie_2 = pd.read_csv(CAMINHO_ARQUIVO_2 , sep=';', encoding='iso-8859-1')
