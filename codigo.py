9738c1058e2687c13b3be37ed3683dac4603ce07
Nicao-ps <nps.guimaraes@gmail.com>
2025-07-08 19:44:26 -0300

    atualiza codigo.py com a análise completa escrita, falta ver o plot dos gráficos

--- codigo.py
+++ codigo.py
@@ -3,7 +3,11 @@ import pandas as pd
 import numpy as np
 
 
-entrada = input('\nInforme a entrada de dados [csv / sql]: ')
+print('\n\n'+'Bem vindo analista em formação!'+'\n')
+
+
+entrada = input('\nInforme o formato desejado para os dados de entrada [csv / sql]: ')
+
 
 try:
 	if entrada == 'csv':
@@ -21,33 +25,61 @@ try:
 		database = 'roubo_coletivo'
 		engine = create_engine(f'mysql+pymysql://{user}:{password}@{host}/{database}')
 		df_serie_1 = pd.read_sql_query('SELECT * FROM basedp', engine)
+		df_serie_1.columns = [col.strip().replace('ï»¿cod_ocorrencia', 'cod_ocorrencia') for col in df_serie_1.columns]
 		df_serie_2 = pd.read_sql_query('SELECT * FROM basedp_roubo_coletivo', engine)
-
-
+		df_serie_2.columns = [col.strip().replace('ï»¿cod_ocorrencia', 'cod_ocorrencia') for col in df_serie_2.columns]
 
 except Exception as e:
 	print(f'Erro! {e}')
 	exit()
 
-print('\nColetando dados para análise...\n')
+
+print('\n\n'+'Coletando dados para análise...'+'\n')
+
+
 df_novo = pd.merge(df_serie_1, df_serie_2, on='cod_ocorrencia')  # on='coluna no dataframe'
 df_novo_filtrado = df_novo[(df_novo['ano'] >= 2022) & (df_novo['ano'] <= 2023)]
-print(df_novo_filtrado)
+filtra_col = ['munic', 'roubo_em_coletivo']
+df_novo_filtrado = df_novo_filtrado[filtra_col].groupby('munic').sum(['roubo_em_coletivo']).sort_values(by='munic', ascending=True).sort_values(by='roubo_em_coletivo', ascending=False).reset_index()
+df_roubo_coletivo = df_novo_filtrado
 
-df_roubo_coletivo = df_novo_filtrado.groupby('munic').sum(['roubo_em_coletivo']).reset_index()
+
+print('\n'+('-'*73))
+print('Análise dos Roubos em Coletivo nos anos de 2022 e de 2023 no Estado do RJ')
+print(('-'*73)+'\n')
+
+
+print(df_roubo_coletivo)
+
 
 array_roubo_coletivo = np.array(df_roubo_coletivo['roubo_em_coletivo'])
 
+
+# medidas de tendência central da série
+print('\n'+('-'*28))
+print('Medidas de Tendência Central')
+print(('-'*28)+'\n')
+
 media = np.mean(array_roubo_coletivo)
-print(media)
+print(f'Média de roubo em coletivos: {media:.2f}')
 
 mediana = np.median(array_roubo_coletivo)
-print(mediana)
+print(f'Mediana de roubo em coletivos: {mediana:.2f}')
+
+
+# medidas de posição da série
+print('\n'+('-'*18))
+print('Medidas de Posição')
+print(('-'*18)+'\n')
+
 
 # quantis da serie
-print('Quantis:\n')
+print(('-'*7))
+print('Quantis')
+print(('-'*7))
+
 list_q = []
-qtt_q = int(input('Quantis utilizados para análise: '))
+qtt_q = int(input('\n'+'Quantis utilizados para análise: '))
 print('')
 for i in range(qtt_q):
 	q = float(input(f'Informe o q{i + 1}: '))
@@ -57,11 +89,16 @@ for i in range(qtt_q):
 	print(f'q{i + 1} = {np.quantile(array_roubo_coletivo, list_q[i], method="weibull"):.2f}')
 print('')
 
+
 # intervalo interquantil
 iqr = np.quantile(array_roubo_coletivo, list_q[qtt_q - 1], method="weibull") - np.quantile(array_roubo_coletivo, list_q[0], method="weibull")
 print(f'Intervalo interquantil: {iqr:.2f}')
 
-print('\nLimites:\n')
+
+# limites da serie
+print('\n'+('-'*7))
+print('Limites')
+print(('-'*7)+'\n')
 
 # limite superior da serie
 mayor_limit = np.quantile(array_roubo_coletivo, list_q[qtt_q - 1], method="weibull") + (1.5 * iqr)
@@ -70,6 +107,8 @@ print(f'Limite superior: {mayor_limit:.2f}')
 # limite inferior da serie
 minor_limit = np.quantile(array_roubo_coletivo, list_q[0], method="weibull") - (1.5 * iqr)
 print(f'Limite inferior: {minor_limit:.2f}')
+
+input('\nImprimir maiores e menores? [s/n]: ')
 
 # maiores da serie
 print('\nMaiores:\n')
